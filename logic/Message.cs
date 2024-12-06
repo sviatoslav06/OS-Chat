ea03e3ef1dc36406d663b2f13a814d565d034199
Святослав Самолюк <samoliuk.sviatoslav@gmail.com>
2024-12-06 14:04:34 +0200

    Initial commit

---
+++ logic/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class Message
+    {
+        public string Text { get; set; } = string.Empty;
+
+        public bool IsReceived { get; set; } // true if message is received from other process
+
+        public DateTime Timestamp { get; set; }
+
+        public Message() { }
+
+        public Message(string text, bool isReceived = false)
+        {
+            Text = text;
+            IsReceived = isReceived;
+            Timestamp = DateTime.Now;
+        }
+    }
+}
